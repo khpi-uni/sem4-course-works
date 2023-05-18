406248dd3beb181f6587071d87b6fd282099e7bd
Vladyslav Storozhenko <vladyslav.storozhenko@boosta.co>
2023-05-18 16:21:21 +0200

    infra v2

--- back/index.js
+++ back/index.js
@@ -1,15 +1,43 @@
 const express = require('express');
+const mysql = require('mysql');
+const {userRouter} = require("./routes/users");
+
 const app = express();
 const port = 3000;
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+    host: 'database', // This refers to the Docker Compose service name of the MySQL database container
+    user: 'db_user',
+    password: 'St*TQ9we2$1m08Qm',
+    database: 'app_db',
 });
+
+// Endpoint to retrieve users
+app.get('/api/users', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error getting MySQL connection:', err);
+            return res.status(500).json({ error: 'Failed to connect to the database' });
+        }
+
+        const query = 'SELECT * FROM users';
+
+        connection.query(query, (error, results) => {
+            connection.release(); // Release the connection back to the pool
+
+            if (error) {
+                console.error('Error executing MySQL query:', error);
+                return res.status(500).json({ error: 'Failed to retrieve users from the database' });
+            }
+
+            res.json(results);
+        });
+    });
+});
+
+app.use('/users', userRouter)
+
 
 // Start the server
 app.listen(port, () => {
