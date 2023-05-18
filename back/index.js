ce5e76155960876b84d009e18a7a27d451c588e0
Vladyslav Storozhenko <vladyslav.storozhenko@boosta.co>
2023-05-18 12:31:15 +0200

    super basic start

---
+++ back/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
