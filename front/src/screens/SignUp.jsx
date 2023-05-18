0d679bfc2bf579a3c62e5dcfeb77a709c591a40c
Pavlo <ano3erone@gmail.com>
2023-05-18 18:53:18 +0300

    feat: add basic navbar

--- front/src/screens/SignUp.jsx
+++ front/src/screens/SignUp.jsx
@@ -1,14 +1,15 @@
 import "./SignUp.css";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Navbar from '../components/Navbar';
 
 const SignUp = () => {
   return (<div>
    <p></p> 
+    <Navbar/>
     <Button>Hello world!</Button>
     <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
   </div>  );
 };
 
 export default SignUp; 
-
