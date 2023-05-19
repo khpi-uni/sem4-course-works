bc2a0f6b07f1513c68cf49a3e7d4a7dee128e0c0
Pavlo <ano3erone@gmail.com>
2023-05-19 18:03:09 +0300

    feat: add expandability to the card

--- front/src/components/Order.jsx
+++ front/src/components/Order.jsx
@@ -1,11 +1,46 @@
 
 import "./Order.css";
-import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
+
+
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 
 const Order = (props) =>{
   const data = props.json_array;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
 <div>
   
@@ -15,13 +50,36 @@ const Order = (props) =>{
 
       <Card sx={{padding: "1em 0.5em"}}>
         <Box sx={{display:"flex",flexDirection:"row",flexGrow:"1"}}>
-      <Box sx={{alignSelf:"start",flexGrow:"1"}}>
+      <Typography sx={{alignSelf:"start",flexGrow:"1"}}>
         {el.order_date}
-      </Box>
-      <Box sx={{justifySelf:"end",}}>
+      </Typography>
+      <Typography sx={{justifySelf:"end",}}>
         {el.total}
+      </Typography>
       </Box>
-      </Box>
+
+  <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography>lots of content</Typography>
+        </CardContent>
+      </Collapse>
+
         </Card>
         ))
       )
