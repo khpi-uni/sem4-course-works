9b6c31e7848d0b7d34e7ef535fcb8d4b13f8fde5
Pavlo <ano3erone@gmail.com>
2023-05-19 14:50:47 +0300

    feat: rm junk

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,21 +1,7 @@
-import {useEffect, useState} from 'react'
+import { useState} from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-/*
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-import SignIn from './screens/SignIn'
-import CheckoutScreen from './screens/CheckoutScreen' 
-*/
-//admin screens
-//
 import SignUp from './screens/SignUp'
 import SignIn from './screens/SignIn'
 import Thanks from './screens/Thanks'
