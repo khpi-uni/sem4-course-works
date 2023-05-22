f2574ccd51f17379c624bdcc6c2c65e91fb50dbc
Vladyslav Storozhenko <vladyslav.storozhenko@boosta.co>
2023-05-22 21:44:10 +0200

    edit user

--- back/users/users.controller.js
+++ back/users/users.controller.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {deleteUser, getUsers} from "./users.service.js";
+import {deleteUser, editUser, getUsers} from "./users.service.js";
 import {passportAdminJWT, passportJWT} from "../auth/helpers/jwt.js";
 
 const usersRouter = express.Router();
 
 usersRouter.get('/get-all', passportAdminJWT, getUsers);
+usersRouter.patch('/edit', passportAdminJWT, editUser);
 usersRouter.delete('/delete', passportAdminJWT, deleteUser);
 
 export {usersRouter};
--- back/users/users.service.js
+++ back/users/users.service.js
@@ -1,5 +1,6 @@
 import {findUserByEmail, omitPassword, retrieveAllUsers} from "./helpers/user.js";
 import {db} from "../db.js";
+import {hashPassword} from "../auth/helpers/password.js";
 
 export const getUsers = async (req, res) => {
     const usersObj = await retrieveAllUsers();
@@ -50,6 +51,65 @@ export const deleteUser = async (req, res) => {
         res.send({
             success: true,
             message: 'successfully deleted the user'
+        })
+    })
+}
+
+
+export const editUser = async (req, res) => {
+    const {email} = req.body;
+
+    if(!email) {
+        res.status(400).send({
+            error: 'email is required'
+        })
+
+        return;
+    }
+
+    const candidate = await findUserByEmail(email);
+
+    if(!candidate) {
+        res.status(400).send({
+            error: 'user with this email does not exist'
+        });
+
+        return;
+    }
+
+    let {newEmail, password: newPassword, shippingAddress: newShippingAddress, billingAddress: newBillingAddress, role: newRole, blacklistStatus: newBlacklistStatus} = req.body;
+
+    // additional validation
+    if(newRole !== 'admin' && newRole !== 'client') {
+        newRole = null;
+    }
+
+    const newUser = {
+        email: newEmail ?? candidate.email,
+        password: newPassword ? hashPassword(newPassword) : candidate.password,
+        shippingAddress: newShippingAddress ?? candidate.shipping_address,
+        billingAddress: newBillingAddress ?? candidate.billing_address,
+        role: newRole ?? candidate.role,
+        blacklistStatus: newBlacklistStatus ?? candidate.is_blacklisted
+    };
+
+    console.log(newUser);
+
+    let sqlQuery = 'UPDATE users SET email = ?, password = ?, shipping_address = ?, billing_address = ?, role = ?, is_blacklisted = ? WHERE id = ?';
+
+    db.query(sqlQuery, [...Object.values(newUser), candidate.id], (err, results) => {
+        if(err) {
+            res.status(400).send({
+                error: err
+            })
+
+            return;
+        }
+
+        res.send({
+            message: 'user was successfully edited',
+            newUser: omitPassword(newUser),
+            results
         })
     })
 }
