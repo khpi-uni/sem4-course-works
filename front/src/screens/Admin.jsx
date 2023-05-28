18909a6d227563c91f7b0dc26d22b048e8b31ef0
Pavlo <ano3erone@gmail.com>
2023-05-28 22:39:15 +0300

    chore: commit before push

--- front/src/screens/Admin.jsx
+++ front/src/screens/Admin.jsx
@@ -1,71 +1,81 @@
 
 
 import "./Thanks.css" 
-
+import {API_HOST} from "../api.js";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 import * as React from 'react';
-
+import {useEffect, useState} from "react";
 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
+
+
+const columns= [
+  {
+    field:"id" 
+  },
+  {
+    field:"created_by_id"
+  },
+  {
+    field:"price" 
+  },
+  {
+    field:"title"
+  },
+  {
+    field:"description" 
+  },
+  {
+    field: "in_stock"
+  },
+  {
+    field: "thumbnail_id" 
+  },
+  {
+    field: "created_at"
+  },
+  {
+    field: "updated_at"
+  },
+]
+
+ 
 
  function DataGridDemo() {
+
+    
+  const [usersInfo, setUsersInfo] = useState(null);
+
+  useEffect(() => {
+    // check if we have token
+
+    (async () => {
+      const response = await fetch(`${API_HOST}/user/get-all`, {headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }})
+
+      console.log(await response.json());
+      setUsersInfo(await response.json())
+    })()
+  }, [])
+
   return (
+    usersInfo?
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={usersInfo}
         columns={columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 8,
             },
           },
         }}
@@ -74,6 +84,7 @@ const rows = [
         disableRowSelectionOnClick
       />
     </Box>
+    :<p>No userinfo</p>
   );
 }
 
