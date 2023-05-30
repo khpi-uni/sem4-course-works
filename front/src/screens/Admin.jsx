427283e5576d41f43189e5bc7b59a423696acfdb
Vladyslav Storozhenko <vladyslav.storozhenko@boosta.co>
2023-05-30 08:35:59 +0200

    fix admin screen:

--- front/src/screens/Admin.jsx
+++ front/src/screens/Admin.jsx
@@ -1,6 +1,4 @@
-
-
-import "./Thanks.css" 
+import "./Thanks.css"
 import {API_HOST} from "../api.js";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -10,99 +8,113 @@ import {useEffect, useState} from "react";
 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { DataGrid } from '@mui/x-data-grid';
+import {DataGrid} from '@mui/x-data-grid';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 
-
-
-
-const columns= [
-  {
-    field:"id" 
-  },
-  {
-    field:"created_by_id"
-  },
-  {
-    field:"price" 
-  },
-  {
-    field:"title"
-  },
-  {
-    field:"description" 
-  },
-  {
-    field: "in_stock"
-  },
-  {
-    field: "thumbnail_id" 
-  },
-  {
-    field: "created_at"
-  },
-  {
-    field: "updated_at"
-  },
+const columns = [
+    {
+        field: "id",
+        headerName: "ID",
+    },
+    {
+        field: "email",
+        headerName: "Email",
+        width: 250
+    },
+    {
+        field: "shipping_address",
+        headerName: "Shipping Address",
+        width: 150,
+        editable: true,
+    },
+    {
+        field: "billing_address",
+        headerName: "Billing Address",
+        width: 150,
+        editable: true
+    },
+    {
+        field: "role",
+        headerName: "Role",
+        editable: true
+    },
+    {
+        headerName: "Delete",
+        renderCell: renderDeleteButton,
+    }
 ]
 
- 
-
- function DataGridDemo() {
-
-    
-  const [usersInfo, setUsersInfo] = useState(null);
-
-  useEffect(() => {
-    // check if we have token
-
-    (async () => {
-      const response = await fetch(`${API_HOST}/user/get-all`, {headers: {
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }})
-
-      console.log(await response.json());
-      setUsersInfo(await response.json())
-    })()
-  }, [])
-
-  return (
-    usersInfo?
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={usersInfo}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 8,
-            },
-          },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
-    :<p>No userinfo</p>
-  );
+function renderDeleteButton() {
+    return (
+        <Button variant="contained" color="error">Delete</Button>
+    );
 }
 
-const Admin = ()=>{
-  
-  return (
 
-    <div>
-      
-    <Navbar/>
-
-    
-    <DataGridDemo/>
+function DataGridDemo() {
 
 
-    <Footer/>
-    </div>
-  );
+    const [usersInfo, setUsersInfo] = useState([]);
+
+    useEffect(() => {
+        // check if we have token
+
+        (async () => {
+            const response = await fetch(`${API_HOST}/user/get-all`, {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+
+            const data = await response.json();
+
+            setUsersInfo(data.users);
+        })()
+    }, [])
+
+    return (
+        usersInfo.length > 0     ?
+            <Box sx={{width: '100%'}}>
+                <DataGrid
+                    rows={usersInfo}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 8,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                    processRowUpdate={(newRow) => {
+                        console.log(newRow)
+                    }}
+                    onProcessRowUpdateError={(e) => {
+                        console.log(e)
+                        console.log('ERROR')
+                    }}
+                />
+            </Box>
+            : <p>No userinfo</p>
+    );
+}
+
+const Admin = () => {
+
+    return (
+        <div>
+            <Navbar/>
+            <Container sx={{margin: "25px auto"}}>
+                <Typography variant={"h4"} mb={1.5}>Users</Typography>
+                <DataGridDemo />
+            </Container>
+            <Footer/>
+        </div>
+    );
 }
 
 
